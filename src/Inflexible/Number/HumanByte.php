d7f9130a1f06a923bce298e54980cd50e9f2c856
Boris Guéry <guery.b@gmail.com>
2013-01-17 00:55:34 +0100

    Implemented HumanByte inflector

---
+++ src/Inflexible/Number/HumanByte.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Inflexible\Number;
+
+use Inflexible\AbstractInflector;
+
+class HumanByte extends AbstractInflector
+{
+    public static function inflect($bytes)
+    {
+        $units = array('B', 'KB', 'MB', 'GB', 'TB');
+        $bytes = max($bytes, 0);
+        $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+        $pow = min($pow, count($units) - 1);
+        $bytes /= (1 << (10 * $pow));
+
+        return sprintf('%4.02f %s', round($bytes, 2), $units[$pow]);
+    }
+}
