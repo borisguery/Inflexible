dae191fdfa4c37793313abb46090674c2acf29d0
Boris Guéry <guery.b@gmail.com>
2013-01-17 18:09:12 +0100

    Ran Licensify inflector (haha) (seriously added license headers)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,11 @@
 <?php
-/**
- * @author Boris Guéry <guery.b@gmail.com>
+/*
+ * This file is part of the Inflexible package.
+ *
+ * (c) Boris Guéry <http://borisguery.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 $file = __DIR__.'/../vendor/autoload.php';
